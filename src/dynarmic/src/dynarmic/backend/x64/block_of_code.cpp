7a878ea926096d359221d8f21d3526ce81081299
lizzie <lizzie@eden-emu.dev>
2026-02-01 03:39:22 +0000

    use dmem for swap buffers, restore full jit sizes

--- src/dynarmic/src/dynarmic/backend/x64/block_of_code.cpp
+++ src/dynarmic/src/dynarmic/backend/x64/block_of_code.cpp
@@ -58,13 +58,8 @@ const std::array<Xbyak::Reg64, ABI_PARAM_COUNT> BlockOfCode::ABI_PARAMS = {Block
 
 namespace {
 
-#ifdef __OPENORBIS__
-constexpr size_t CONSTANT_POOL_SIZE = 8 * 4096;
-constexpr size_t PRELUDE_COMMIT_SIZE = 8 * 4096;
-#else
 constexpr size_t CONSTANT_POOL_SIZE = 2 * 1024 * 1024;
 constexpr size_t PRELUDE_COMMIT_SIZE = 16 * 1024 * 1024;
-#endif
 
 #ifdef DYNARMIC_ENABLE_NO_EXECUTE_SUPPORT
 void ProtectMemory(const void* base, size_t size, bool is_executable) {
