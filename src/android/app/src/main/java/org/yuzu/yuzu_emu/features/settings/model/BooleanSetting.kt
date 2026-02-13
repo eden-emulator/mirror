rorrim-rorrim/eden-alt

Revert "[vk] Unify RAII in Vulkan (#2679)"
* properly use RENDERER_ENABLE_RAII
* keep it ON as default

This reverts commit db65f107689f853141fd2cf4cb7b6b55b24aff3e.

src/android/app/src/main/java/org/yuzu/yuzu_emu/features/settings/model/BooleanSetting.kt
@@ -23,6 +23,7 @@ enum class BooleanSetting(override val key: String) : AbstractBooleanSetting {
     RENDERER_FORCE_MAX_CLOCK("force_max_clock"),
     RENDERER_ASYNCHRONOUS_SHADERS("use_asynchronous_shaders"),
     RENDERER_REACTIVE_FLUSHING("use_reactive_flushing"),
+    RENDERER_ENABLE_RAII("enable_raii"),
     SYNC_MEMORY_OPERATIONS("sync_memory_operations"),
     BUFFER_REORDER_DISABLE("disable_buffer_reorder"),
     RENDERER_DEBUG("debug"),
@@ -71,9 +72,7 @@ enum class BooleanSetting(override val key: String) : AbstractBooleanSetting {
     DONT_SHOW_DRIVER_SHADER_WARNING("dont_show_driver_shader_warning"),
     ENABLE_OVERLAY("enable_overlay");
 
-
-//  external fun isFrameSkippingEnabled(): Boolean
-    external fun isFrameInterpolationEnabled(): Boolean
+    external fun isRaiiEnabled(): Boolean
 
     override fun getBoolean(needsGlobal: Boolean): Boolean =
         NativeConfig.getBoolean(key, needsGlobal)

src/android/app/src/main/java/org/yuzu/yuzu_emu/features/settings/model/view/SettingsItem.kt
@@ -231,7 +231,6 @@ abstract class SettingsItem(
 
                 override fun reset() = BooleanSetting.USE_DOCKED_MODE.reset()
             }
-
             put(
                 SwitchSetting(
                     dockedModeSetting,
@@ -630,6 +629,14 @@ abstract class SettingsItem(
                     valuesId = R.array.optimizeSpirvOutputValues
                 )
             )
+            put(
+                SwitchSetting(
+                    BooleanSetting.RENDERER_ENABLE_RAII,
+                    titleId = R.string.enable_raii,
+                    descriptionId = R.string.enable_raii_description
+                )
+            )
+
             put(
                 SingleChoiceSetting(
                     IntSetting.DMA_ACCURACY,

src/video_core/vulkan_common/vulkan_wrapper.cpp
@@ -12,6 +12,7 @@
 
 #include "common/common_types.h"
 #include "common/logging/log.h"
+#include "common/settings.h"
 #include "video_core/vulkan_common/vk_enum_string_helper.h"
 #include "video_core/vulkan_common/vma.h"
 #include "video_core/vulkan_common/vulkan_wrapper.h"
@@ -310,7 +311,10 @@ const char* Exception::what() const noexcept {
 }
 
 void Destroy(VkInstance instance, const InstanceDispatch& dld) noexcept {
-    dld.vkDestroyInstance(instance, nullptr);
+    // FIXME: A double free occurs here if RAII is enabled.
+    if (!Settings::values.enable_raii.GetValue()) {
+        dld.vkDestroyInstance(instance, nullptr);
+    }
 }
 
 void Destroy(VkDevice device, const InstanceDispatch& dld) noexcept {
@@ -413,7 +417,10 @@ void Destroy(VkInstance instance, VkDebugReportCallbackEXT handle,
 }
 
 void Destroy(VkInstance instance, VkSurfaceKHR handle, const InstanceDispatch& dld) noexcept {
-    dld.vkDestroySurfaceKHR(instance, handle, nullptr);
+    // FIXME: A double free occurs here if RAII is enabled.
+    if (!Settings::values.enable_raii.GetValue()) {
+        dld.vkDestroySurfaceKHR(instance, handle, nullptr);
+    }
 }
 
 VkResult Free(VkDevice device, VkDescriptorPool handle, Span<VkDescriptorSet> sets,

eden-emulator/mirror

src/android/app/src/main/java/org/yuzu/yuzu_emu/features/settings/model/BooleanSetting.kt
@@ -25,6 +25,7 @@ enum class BooleanSetting(override val key: String) : AbstractBooleanSetting {
     RENDERER_ASYNCHRONOUS_SHADERS("use_asynchronous_shaders"),
     RENDERER_REACTIVE_FLUSHING("use_reactive_flushing"),
     ENABLE_BUFFER_HISTORY("enable_buffer_history"),
+    RENDERER_ENABLE_RAII("renderer_enable_raii"),
     SYNC_MEMORY_OPERATIONS("sync_memory_operations"),
     BUFFER_REORDER_DISABLE("disable_buffer_reorder"),
     RENDERER_DEBUG("debug"),
@@ -86,8 +87,7 @@ enum class BooleanSetting(override val key: String) : AbstractBooleanSetting {
 
     ENABLE_QUICK_SETTINGS("enable_quick_settings");
 
-//  external fun isFrameSkippingEnabled(): Boolean
-    external fun isFrameInterpolationEnabled(): Boolean
+    external fun isRaiiEnabled(): Boolean
 
     override fun getBoolean(needsGlobal: Boolean): Boolean =
         NativeConfig.getBoolean(key, needsGlobal)

src/android/app/src/main/java/org/yuzu/yuzu_emu/features/settings/model/view/SettingsItem.kt
@@ -655,6 +655,14 @@ abstract class SettingsItem(
                 )
             )
             put(
+                SwitchSetting(
+                    BooleanSetting.RENDERER_ENABLE_RAII,
+                    titleId = R.string.renderer_enable_raii,
+                    descriptionId = R.string.renderer_enable_raii_description
+                )
+            )
+
+            put(
                 SingleChoiceSetting(
                     IntSetting.DMA_ACCURACY,
                     titleId = R.string.dma_accuracy,

src/video_core/vulkan_common/vulkan_wrapper.cpp
@@ -12,6 +12,7 @@
 
 #include "common/common_types.h"
 #include "common/logging/log.h"
+#include "common/settings.h"
 #include "video_core/vulkan_common/vk_enum_string_helper.h"
 #include "video_core/vulkan_common/vma.h"
 #include "video_core/vulkan_common/vulkan_wrapper.h"
@@ -313,7 +314,10 @@ const char* Exception::what() const noexcept {
 }
 
 void Destroy(VkInstance instance, const InstanceDispatch& dld) noexcept {
-    dld.vkDestroyInstance(instance, nullptr);
+    // FIXME: A double free occurs here if RAII is enabled.
+    if (!Settings::values.renderer_enable_raii.GetValue()) {
+        dld.vkDestroyInstance(instance, nullptr);
+    }
 }
 
 void Destroy(VkDevice device, const InstanceDispatch& dld) noexcept {
@@ -416,7 +420,10 @@ void Destroy(VkInstance instance, VkDebugReportCallbackEXT handle,
 }
 
 void Destroy(VkInstance instance, VkSurfaceKHR handle, const InstanceDispatch& dld) noexcept {
-    dld.vkDestroySurfaceKHR(instance, handle, nullptr);
+    // FIXME: A double free occurs here if RAII is enabled.
+    if (!Settings::values.renderer_enable_raii.GetValue()) {
+        dld.vkDestroySurfaceKHR(instance, handle, nullptr);
+    }
 }
 
 VkResult Free(VkDevice device, VkDescriptorPool handle, Span<VkDescriptorSet> sets,