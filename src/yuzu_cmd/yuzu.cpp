c32c197dea0b9bc4868d1012119371c5844f90b2
lizzie <lizzie@eden-emu.dev>
2026-02-25 15:25:16 +0000

    disable stdio buffering

--- src/yuzu_cmd/yuzu.cpp
+++ src/yuzu_cmd/yuzu.cpp
@@ -195,6 +195,11 @@ extern "C" SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
         freopen("CONOUT$", "wb", stderr);
     }
 #endif
+#ifdef __OPENORBIS__
+    // May prevent spurious crashes on swap handlers...
+    setvbuf(stdout, nullptr, _IONBF, 0);
+    setvbuf(stderr, nullptr, _IONBF, 0);
+#endif
 
     Common::Log::Initialize();
     Common::Log::SetColorConsoleBackendEnabled(true);
