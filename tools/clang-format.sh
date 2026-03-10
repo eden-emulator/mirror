ffc20ff93f970a5581fc2178752d10d821426854
crueter <crueter@eden-emu.dev>
2026-03-10 00:06:50 -0400

    update clang-format
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- tools/clang-format.sh
+++ tools/clang-format.sh
@@ -3,4 +3,5 @@
 # SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-exec find src -iname "*.h" -o -iname "*.cpp" | xargs clang-format -i -style=file:src/.clang-format
+# Only clang-formats Qt stuff. :)
+find src/qt_common src/yuzu -iname "*.h" -o -iname "*.cpp" | xargs clang-format -i -style=file:src/.clang-format
