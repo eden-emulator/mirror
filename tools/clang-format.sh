d408ba6860a01c0bb3636cc12ed1283325ae3615
crueter <crueter@eden-emu.dev>
2026-03-10 00:05:03 -0400

    Fix license headers

--- tools/clang-format.sh
+++ tools/clang-format.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-# SPDX-FileCopyrightText: Copyright 2025 Eden Emulator Project
+# SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 exec find src -iname "*.h" -o -iname "*.cpp" | xargs clang-format -i -style=file:src/.clang-format
