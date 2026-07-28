f6c1404e09b839bc972e7e8ad5f32fc2a306e635
CamilleLaVey <camillelavey99@gmail.com>
2026-07-28 01:24:06 -0400

    Revert "[TEST] Some miscellaneous changes within buffers queue and RAII deallocations"

--- src/core/hle/service/nvdrv/nvdrv_interface.cpp
+++ src/core/hle/service/nvdrv/nvdrv_interface.cpp
@@ -5,8 +5,6 @@
 // SPDX-FileCopyrightText: 2021 Skyline Team and Contributors
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#include <algorithm>
-
 #include "common/logging.h"
 #include "common/scope_exit.h"
 #include "common/string_util.h"
@@ -20,17 +18,6 @@
 #include "core/hle/service/nvdrv/nvdrv_interface.h"
 
 namespace Service::Nvidia {
-
-namespace {
-
-size_t ClampIoctlOutputSize(size_t descriptor_size, u32 ioctl_length) {
-    if (ioctl_length == 0) {
-        return descriptor_size;
-    }
-    return (std::min)(descriptor_size, static_cast<size_t>(ioctl_length));
-}
-
-}
 
 void NVDRV::Open(HLERequestContext& ctx) {
     LOG_DEBUG(Service_NVDRV, "called");
@@ -81,8 +68,7 @@ void NVDRV::Ioctl1(HLERequestContext& ctx) {
     }
 
     // Check device
-    output_buffer.resize_destructive(
-        ClampIoctlOutputSize(ctx.GetWriteBufferSize(0), command.length));
+    output_buffer.resize_destructive(ctx.GetWriteBufferSize(0));
     const auto input_buffer = ctx.ReadBuffer(0);
 
     const auto nv_result = nvdrv->Ioctl1(fd, command, input_buffer, output_buffer);
@@ -109,8 +95,7 @@ void NVDRV::Ioctl2(HLERequestContext& ctx) {
 
     const auto input_buffer = ctx.ReadBuffer(0);
     const auto input_inlined_buffer = ctx.ReadBuffer(1);
-    output_buffer.resize_destructive(
-        ClampIoctlOutputSize(ctx.GetWriteBufferSize(0), command.length));
+    output_buffer.resize_destructive(ctx.GetWriteBufferSize(0));
 
     const auto nv_result =
         nvdrv->Ioctl2(fd, command, input_buffer, input_inlined_buffer, output_buffer);
@@ -136,8 +121,7 @@ void NVDRV::Ioctl3(HLERequestContext& ctx) {
     }
 
     const auto input_buffer = ctx.ReadBuffer(0);
-    output_buffer.resize_destructive(
-        ClampIoctlOutputSize(ctx.GetWriteBufferSize(0), command.length));
+    output_buffer.resize_destructive(ctx.GetWriteBufferSize(0));
     inline_output_buffer.resize_destructive(ctx.GetWriteBufferSize(1));
 
     const auto nv_result =
--- src/core/hle/service/nvnflinger/buffer_queue_producer.h
+++ src/core/hle/service/nvnflinger/buffer_queue_producer.h
@@ -81,7 +81,6 @@ private:
 
     Kernel::KEvent* buffer_wait_event{};
     Service::KernelHelpers::ServiceContext& service_context;
-    std::shared_ptr<IProducerListener> internal_listener;
 
     std::shared_ptr<BufferQueueCore> core;
     BufferQueueDefs::SlotsType& slots;
