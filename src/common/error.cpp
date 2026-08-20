rorrim-rorrim/eden-alt

fix strerror_r on managarm

src/common/error.cpp
@@ -30,7 +30,7 @@ std::string NativeErrorToString(int e) {
     return ret;
 #else
     char err_str[255];
-#if defined(ANDROID) ||                                                                            \
+#if defined(__managarm__) || defined(ANDROID) ||                                                                            \
     (defined(__GLIBC__) && (_GNU_SOURCE || (_POSIX_C_SOURCE < 200112L && _XOPEN_SOURCE < 600)))
     // Thread safe (GNU-specific)
     const char* str = strerror_r(e, err_str, sizeof(err_str));

eden-emulator/mirror

src/common/error.cpp
@@ -33,7 +33,7 @@ std::string NativeErrorToString(int e) {
     return ret;
 #else
     char err_str[255];
-#if defined(__ANDROID__) ||                                                                            \
+#if defined(__managarm__) || defined(__ANDROID__) ||                                                                            \
     (defined(__GLIBC__) && (_GNU_SOURCE || (_POSIX_C_SOURCE < 200112L && _XOPEN_SOURCE < 600)))
     // Thread safe (GNU-specific)
     const char* str = strerror_r(e, err_str, sizeof(err_str));