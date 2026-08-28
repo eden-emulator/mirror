75519010d2866f20025a90269b9a13e65db505bd
CamilleLaVey <camillelavey99@gmail.com>
2026-08-28 14:28:09 -0400

    Refining fix

--- src/video_core/texture_cache/texture_cache.h
+++ src/video_core/texture_cache/texture_cache.h
@@ -1122,6 +1122,11 @@ void TextureCache<P>::RefreshContents(Image& image, ImageId image_id) {
 
     TrackImage(image, image_id);
 
+    if (True(image.flags & ImageFlagBits::GpuModified) && image.info.rescaleable) {
+        runtime.TransitionImageLayout(image);
+        return;
+    }
+
     if (image.info.num_samples > 1 && !runtime.CanUploadMSAA()) {
         LOG_WARNING(HW_GPU, "MSAA image uploads are not implemented");
         runtime.TransitionImageLayout(image);
