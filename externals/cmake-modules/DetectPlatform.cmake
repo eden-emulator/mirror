rorrim-rorrim/eden-alt

initial wasm support

externals/cmake-modules/DetectPlatform.cmake
@@ -33,6 +33,9 @@ elseif (${CMAKE_SYSTEM_NAME} STREQUAL "Haiku")
     set(PLATFORM_HAIKU ON)
 elseif (${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
     set(PLATFORM_LINUX ON)
+elseif (${CMAKE_SYSTEM_NAME} STREQUAL "Emscripten")
+    set(PLATFORM_EMSCRIPTEN ON)
+    message(WARNING "${CMAKE_LIBRARY_ARCHITECTURE} support is highly experimental!!!")
 endif()
 
 # dumb heuristic to detect msys2

eden-emulator/mirror

externals/cmake-modules/DetectPlatform.cmake
@@ -20,6 +20,9 @@ elseif (${CMAKE_SYSTEM_NAME} STREQUAL "managarm")
     set(MANAGARM ON)
 elseif (${CMAKE_SYSTEM_NAME} STREQUAL "Haiku")
     set(HAIKUOS ON)
+elseif (${CMAKE_SYSTEM_NAME} STREQUAL "Emscripten")
+    set(EMSCRIPTEN ON)
+    message(WARNING "${CMAKE_LIBRARY_ARCHITECTURE} support is highly experimental!!!")
 endif()
 
 # BSD