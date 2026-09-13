5d150cac5c6624fa6a0e94266846b75d91a6a5d0
PavelBARABANOV <pavelbarabanov94@gmail.com>
2026-09-13 02:13:06 +0200

    [dynarmic, am, android, ns] Fixes the latest qlaunch regressions on Android and adds cmd23 for launching games on FW 23 (#4407)
    - [x] I have read and followed the [Contribution Guidelines](https://git.eden-emu.dev/eden-emu/eden/src/branch/master/CONTRIBUTING.md#code-contributions).
    - [x] I have read and followed the [AI Policy](https://git.eden-emu.dev/eden-emu/eden/src/branch/master/docs/policies/AI.md)
    - [x] I have read and followed the [Coding Guidelines](https://git.eden-emu.dev/eden-emu/eden/src/branch/master/docs/policies/Coding.md) to the best of my ability.
    
    -------------------
    [dynarmic] use TST + B.NE instead of TBNZ for marked bit
    Fixes qlaunch and ac3 crashes.
    
    [am] Bruno FIX OVERLAY 2.0
    Fixes incorrect load display when the overlay applet is enabled in AC3.
    
    [android] handle empty programId in Game.settingsName
    The ReShade post-processing change started calling
    SettingsFile.loadCustomConfig() on every drawer open through
    withPerGameConfig(), which exposed a pre-existing crash in
    Game.settingsName when programId was an empty string.
    
    [ns] use GetApplicationControlData3 for cmd23 in IReadOnlyApplicationControlDataInterface
    We can use this method for now for launching games on fw23, while we don't have all the information.
    
    Reviewed-on: https://git.eden-emu.dev/eden-emu/eden/pulls/4407
    Reviewed-by: lizzie <lizzie@eden-emu.dev>
    Reviewed-by: CamilleLaVey <camillelavey99@gmail.com>

--- src/android/app/src/main/java/org/yuzu/yuzu_emu/model/Game.kt
+++ src/android/app/src/main/java/org/yuzu/yuzu_emu/model/Game.kt
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright 2025 Eden Emulator Project
+// SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 // SPDX-FileCopyrightText: 2023 yuzu Emulator Project
@@ -37,7 +37,7 @@ class Game(
 
     val settingsName: String
         get() {
-            val programIdLong = programId.toLong()
+            val programIdLong = programId.toLongOrNull() ?: 0L
             return if (programIdLong == 0L) {
                 FileUtil.getFilename(Uri.parse(path))
             } else {
@@ -47,7 +47,7 @@ class Game(
 
     val programIdHex: String
         get() {
-            val programIdLong = programId.toLong()
+            val programIdLong = programId.toLongOrNull() ?: 0L
             return if (programIdLong == 0L) {
                 "0"
             } else {
