4514e654552c7d65d14081fcf0f3a0d6515026de
lizzie <lizzie@eden-emu.dev>
2026-09-15 08:14:15 +0000

    2026-09-15 08:14:15
    Signed-off-by: lizzie <lizzie@eden-emu.dev>

--- src/core/hle/kernel/k_slab_heap.h
+++ src/core/hle/kernel/k_slab_heap.h
@@ -12,7 +12,6 @@
 #include "common/atomic_ops.h"
 #include "common/common_funcs.h"
 #include "common/common_types.h"
-#include "common/spin_lock.h"
 
 namespace Kernel {
 
@@ -68,7 +67,7 @@ public:
 
 private:
     std::atomic<Node*> m_head{};
-    Common::SpinLock m_lock;
+    std::mutex m_lock;
 };
 
 } // namespace impl
